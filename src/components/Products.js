95c5f519676e728bf3ed8182913b0117466cc972
Austin Rowe <amrowe2018@gmail.com>
2019-06-25 18:47:23 -0500

    Removed green logo merch and added red logo hat and blue logo red shirt.

--- src/components/Products.js
+++ src/components/Products.js
@@ -8,12 +8,9 @@ import ProductTile from './ProductTile';
 const Products = (props) => {
     return ( 
         <div id="products-container">
-            <ProductTile id={16} title="INDICTMENT CLOTHING HAT" dispatch={props.dispatch} imgSrc="short-sleeve-edits/GreenIndictmentLogoCap.jpg" description='GREEN ON BLACK' itemType="HAT" price={24} />
-            <ProductTile id={1} title="TICK TOCK HOURGLASS T-SHIRT" dispatch={props.dispatch} imgSrc="short-sleeve-edits/Green_Black.jpg" description='GREEN ON BLACK' itemType="SHIRT" price={28} />
-            <ProductTile id={2} title="TICK TOCK HOURGLASS T-SHIRT" dispatch={props.dispatch} imgSrc="short-sleeve-edits/Green_Blue.jpg" description='GREEN ON BLUE' itemType="SHIRT" price={28} />
-            <ProductTile id={3} title="TICK TOCK HOURGLASS T-SHIRT" dispatch={props.dispatch} imgSrc="short-sleeve-edits/Green_Gray.jpg" description='GREEN ON GRAY' itemType="SHIRT" price={28} />
-            <ProductTile id={4} title="TICK TOCK HOURGLASS T-SHIRT" dispatch={props.dispatch} imgSrc="short-sleeve-edits/Green_Teal.jpg" description='GREEN ON TURQUOISE' itemType="SHIRT" price={28} />
-            <ProductTile id={5} title="TICK TOCK HOURGLASS T-SHIRT" dispatch={props.dispatch} imgSrc="short-sleeve-edits/Green_White.jpg" description='GREEN ON WHITE' itemType="SHIRT" price={28} />
+            <ProductTile id={17} title="INDICTMENT CLOTHING HAT" dispatch={props.dispatch} imgSrc="short-sleeve-edits/RedIndictmentLogoCap.jpg" description='RED ON BLACK' itemType="HAT" price={24} />
+            <ProductTile id={18} title="TICK TOCK HOURGLASS T-SHIRT" dispatch={props.dispatch} imgSrc="short-sleeve-edits/JusticeMockUp.jpg" description='BLUE ON RED' itemType="SHIRT" price={28} />
+
             <ProductTile id={6} title="TICK TOCK HOURGLASS T-SHIRT" dispatch={props.dispatch} imgSrc="short-sleeve-edits/Orange_Black.jpg" description='ORANGE ON BLACK' itemType="SHIRT" price={28} />
             <ProductTile id={7} title="TICK TOCK HOURGLASS T-SHIRT" dispatch={props.dispatch} imgSrc="short-sleeve-edits/Orange_Blue.jpg" description='ORANGE ON BLUE' itemType="SHIRT" price={28} />
             <ProductTile id={8} title="TICK TOCK HOURGLASS T-SHIRT" dispatch={props.dispatch} imgSrc="short-sleeve-edits/Orange_Gray.jpg" description='ORANGE ON GRAY' itemType="SHIRT" price={28} />
